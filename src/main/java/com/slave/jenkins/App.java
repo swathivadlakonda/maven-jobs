226a564363ffd0e79a668e405acdd685ec9a1bdf
swathivadlakonda <52874669+swathivadlakonda@users.noreply.github.com>
2019-08-26 23:26:02 +0530

    changed

--- src/main/java/com/slave/jenkins/App.java
+++ src/main/java/com/slave/jenkins/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is app.javaaaaagg ....!!!!***" );
+        System.out.println( "this is app.java...!!!!***" );
     }
 }
