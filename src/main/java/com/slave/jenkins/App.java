0281ed892ba0f68d95dca3d0619bbabf5dbe8cbc
swathivadlakonda <52874669+swathivadlakonda@users.noreply.github.com>
2019-08-29 07:50:49 +0530

    dfgfgg

--- src/main/java/com/slave/jenkins/App.java
+++ src/main/java/com/slave/jenkins/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is app.java file....!!!!" );
+        System.out.println( "this is app.java file.....!!!!" );
     }
 }
